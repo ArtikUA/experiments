c338c8430b2ab0af6d8d084233850e41109a590e
Artik <artik.ua@gmail.com>
2016-05-10 12:57:42 +0200

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,20 @@
+from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
+
+
+class MyView(TemplateView):
+    template_name = '1.html'
+
+    def get_context_data(self, article_id):
+        context = super(MyView, self).get_context_data()
+        print(context)
+        print(self)
+        context['foo'] = article_id
+        return context
+
+
+class RedView(RedirectView):
+    def get_redirect_url(self, id):
+        id = int(id)
+        return reverse('article-detail', kwargs={'article_id': id})
---
+++ experiments/urls.py
@@ -0,0 +1,28 @@
+"""experiments URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from app.views import MyView, RedView
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'abc/(?P<article_id>\d+)/$', MyView.as_view(), name='article-detail'),
+    url(r'red/(?P<id>\d+)/$', RedView.as_view()),
+]
+
+urlpatterns += staticfiles_urlpatterns()
